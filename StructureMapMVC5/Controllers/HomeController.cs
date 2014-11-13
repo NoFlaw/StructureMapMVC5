0b76a1be809ec01d90d4c9cdf45acd0f4887e2f5
NoFlaw <brentonbates@gmail.com>
2014-11-12 22:48:33 -0500

    Initial Check-In of codebase for StructureMapMvc5 sample

---
+++ StructureMapMVC5/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StructureMapMVC5.Models;
+
+namespace StructureMapMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMessageFactory _messageFactory;
+
+        public HomeController(IMessageFactory messageFactory)
+        {
+            _messageFactory = messageFactory;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = _messageFactory.GetMessage();
+            return View();
+        }
+
+    }
+}
---
+++ StructureMapMVC5/Models/MessageFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StructureMapMVC5.Models
+{
+    public interface IMessageFactory
+    {
+        string GetMessage();
+    }
+    public class MessageFactory : IMessageFactory
+    {
+        public string GetMessage()
+        {
+            return "Class: " + GetType().FullName;
+        }
+    }
+}
